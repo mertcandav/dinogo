036f0e8b5dc8d1c30715c0ab8b2dfd345035a7b6
mertcandav <mertcandav@outlook.com>
2021-08-28 17:20:00 +0300

    Add navigation escape sequences.

---
+++ ansiescape/pos.go
@@ -0,0 +1,57 @@
+package ansiescape
+
+import "fmt"
+
+const (
+	// SavePosition is save cursor position.
+	SavePosition = "\033[s"
+	// RestorePosition is restore cursor position.
+	RestorePosition = "\033[u"
+)
+
+// MoveUp returns sequence for move the cursor up N lines.
+func MoveUp(n int) string {
+	return fmt.Sprintf("\033[%dA", n)
+}
+
+// MoveDown returns sequence for move the cursor down N lines.
+func MoveDown(n int) string {
+	return fmt.Sprintf("\033[%dB", n)
+}
+
+// MoveBack returns sequence for move the cursor backward N columns.
+func MoveLeft(n int) string {
+	return fmt.Sprintf("\033[%dD", n)
+}
+
+// MoveBack returns sequence for move the cursor backward N columns.
+func MoveRight(n int) string {
+	return fmt.Sprintf("\033[%dD", n)
+}
+
+// NextLine returns sequence for move cursor to
+// beginning of line N lines down.
+func NextLine(n int) string {
+	return fmt.Sprintf("\u001b[%dE", n)
+}
+
+// PrevLine returns sequence for move cursor to
+// beginning of line N lines up.
+func PrevLine(n int) string {
+	return fmt.Sprintf("\u001b[%dF", n)
+}
+
+// SetColumn returns sequence for move cursor to column N.
+func SetColumn(n int) string {
+	return fmt.Sprintf("\u001b[%dG", n)
+}
+
+// SetPosition returns sequence for puts the cursor at line and column.
+func SetPosition(line, column int) string {
+	return fmt.Sprintf("\033[%d;%dH", line, column)
+}
+
+// SetTitle returns sequence for set title of window.
+func SetTitle(title string) string {
+	return fmt.Sprintf("\033]0;%s\007", title)
+}
--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/mertcandav/dinogo/ansiescape"
+	"github.com/mertcandav/dinogo/input"
 )
 
 func main() {
-	fmt.Print(ansiescape.ClearScreen)
+	fmt.Print(ansiescape.PrevLine(1))
+	_, _ = input.ReadRune()
 }
