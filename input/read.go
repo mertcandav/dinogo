99a1915aed0468d3e9e7ce57b27e08ce8706e5da
mertcandav <mertcandav@outlook.com>
2021-08-28 14:33:26 +0300

    Add input package.

---
+++ input/read.go
@@ -0,0 +1,25 @@
+package input
+
+import (
+	"github.com/eiannone/keyboard"
+)
+
+// ReadRune reads one char from command line
+// without press enter.
+func ReadRune() (rune, error) {
+	for {
+		r, key, err := keyboard.GetSingleKey()
+		if err != nil {
+			return 0, err
+		}
+		switch key {
+		case 9: // tab
+			r = '\t'
+		case 32: // space
+			r = ' '
+		}
+		if r >= 0 {
+			return r, nil
+		}
+	}
+}
