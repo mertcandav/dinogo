dad00cb6da9bdcec936447b8fc719e015565798f
mertcandav <mertcandav@outlook.com>
2021-08-28 16:09:18 +0300

    Add line clear constants.

---
+++ ansiescape/line.go
@@ -0,0 +1,10 @@
+package ansiescape
+
+const (
+	// ClearLine clears entire line.
+	ClearLin = "\u001b[2K"
+	// ClearLineCB clears from cursor to start of line.
+	ClearLineCB = "\u001b[1K"
+	// ClearLineCE clears from cursor to end of line.
+	ClearLineCE = "\u001b[0K"
+)
