8112e17323e1160705d30c2fd119c2afccf74252
mertcandav <mertcandav@outlook.com>
2021-09-03 12:26:50 +0300

    Add show/hide escape sequences for cursor to ansiescape package and ShowCursor/HideCursor functions to terminal package.

---
+++ ansiescape/cursor.go
@@ -0,0 +1,7 @@
+package ansiescape
+
+// Escape sequences for show/hide cursor.
+const (
+	ShowCursor = "\033[?25h"
+	HideCursor = "\033[?25l"
+)
---
+++ terminal/cursor.go
@@ -0,0 +1,13 @@
+package terminal
+
+import "github.com/mertcandav/dinogo/ansiescape"
+
+// ShowCursor shows cursor.
+func ShowCursor() {
+	print(ansiescape.ShowCursor)
+}
+
+// HideCursor hides cursor.
+func HideCursor() {
+	print(ansiescape.HideCursor)
+}
